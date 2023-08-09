e99d3d2c961b66510f2ce39efe476dff7378557e
Jonathan Anderson <anderjon@oregonstate.edu>
2023-08-09 18:07:22 +0000

    Deleted some moved files

--- hashes/add_to_existing_hash.rb
+++
@@ -1,3 +0,0 @@
-person = { height: '6 ft', weight: '160 lbs' }
-
-person[:age] = 62
--- hashes/delete_from_existing_hash.rb
+++
@@ -1,5 +0,0 @@
-person = { height: '6ft', weight: '160 lbs' }
-
-person[:age] = 62
-
-person.delete(:age) # returns 62
