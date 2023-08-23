f3e6e44f3fdf83a02c278efa38f1fbc2e1b445c9
Jonathan Anderson <anderjon@oregonstate.edu>
2023-08-23 12:49:24 +0000

    added code to nil file, operations file created

--- the_basics/nil.rb
+++ the_basics/nil.rb
@@ -1,1 +1,23 @@
 puts "Hello, world!" # returns nil
+
+# You can explicitly refer to nil
+x = nil # => nil
+
+# It is possible to check if something is a nil type by using .nil?
+"Hello, World".nil? # => false
+
+# nil will be treated as false when used as an expression
+if nil
+  puts "Hello, World!"
+end 
+# => nil
+
+# Opposing
+if 1
+  puts "Hello, World!"
+end 
+# => prints "Hello, World!" -- returns nil
+
+
+# important caveat
+false == nil # => false
