8e350b34512395c5d53ef05b51c5ea4c6b974be1
Jonathan Anderson <anderjon@oregonstate.edu>
2023-08-23 15:31:55 +0000

    created basic data structures file, removed content from operations file

---
+++ the_basics/basic_data_structures.rb
@@ -0,0 +1,16 @@
+# Basic Data Structures
+
+# Arrays
+[1, 2, 3, 4, 5] # => [1, 2, 3, 4, 5]
+
+# Get first element
+[1, 2, 3, 4, 5][0] # => 1
+
+
+# Hashes
+{ :dog => 'barks' } # => { :dogs => 'barks' }
+
+{ :dog => 'barks', :cat => 'meows', :pig => 'oinks' } 
+# => { :dog => 'barks', :cat => 'meows', :pig => 'oinks' }
+
+{ :dog => 'barks', :cat => 'meows', :pig => 'oinks' }[:cat] # => "meows"
