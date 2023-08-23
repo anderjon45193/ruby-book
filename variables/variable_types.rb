cec5d506f62811e04b0b0f2073f7364c0746d060
Jonathan Anderson <anderjon@oregonstate.edu>
2023-08-23 17:19:53 +0000

    added variable type declaration code snippets

---
+++ variables/variable_types.rb
@@ -0,0 +1,14 @@
+# Constant declaraton
+MY_CONSTANT = 'I am available throughout your app.'
+
+# Global variable declaraton
+$var = 'I am also available throught your app.'
+
+# Class variable
+@@instances = 0
+
+# Instance variable
+@var = 'I am available throught the current instance of this class.'
+
+# Local variable
+var = 'I must be passed around to cross scope boundaries'
