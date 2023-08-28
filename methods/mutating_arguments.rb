810c1757736fce3ff747fe70266beb819e736cf8
Jonathan Anderson <anderjon@oregonstate.edu>
2023-08-28 13:56:36 +0000

    updated mutating arguments file, and added return file

--- methods/mutating_arguments.rb
+++ methods/mutating_arguments.rb
@@ -6,3 +6,25 @@ a = 5
 some_method(a)
 puts a # 5, number is scoped at the method definition level.
 # a's value is unchanged
+
+a = [1, 2, 3]
+
+# Example of a method definition that mutates its argument permanently
+def mutate(array)
+  array.pop
+end 
+
+p "Befure mutate method: #{a}"
+mutate(a)
+p "After mutate method: #{a}"
+
+a = [1, 2, 3]
+
+# Example of a method definition that does not mutate the argument
+def no_mutate(array)
+  array.last
+end 
+
+p "Before no_mutate method: #{a}"
+no_mutate(a)
+p "After no_mutate method: #{a}"
