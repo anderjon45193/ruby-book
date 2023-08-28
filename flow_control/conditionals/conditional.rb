1898e29c497d6a4a618c9895ec083fd5e9db0175
Jonathan Anderson <anderjon@oregonstate.edu>
2023-08-28 18:54:37 +0000

    conditional file added

--- flow_control/conditionals/conditional.rb
+++ flow_control/conditionals/conditional.rb
@@ -10,3 +10,31 @@ elsif a == 4
 else
   puts "a is neither 3, nor 4"
 end
+
+# Example 1
+if x == 3
+  puts "x is 3"
+end
+
+# Example 2
+if x == 3
+  puts "x is 3"
+elsif x == 4
+  puts "x is 4"
+end
+
+# Example 3
+if x == 3
+  puts "x is 3"
+else
+  puts "x is NOT 3"
+end
+
+# Example 4
+# must use then keyword when using
+# 1-line syntax
+if x == 3 then puts "x is 3" end
+  
+puts "x is 3" if x == 3
+
+puts "x is NOT 3" unless x == 3
